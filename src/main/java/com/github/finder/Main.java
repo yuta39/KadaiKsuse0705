f3c9e75fd30cc460b8d6d082ae73fba998a6afe3
yuta39 <xoggkllo00@gmail.com>
2015-07-01 01:13:35 +0900

    refs #1 Mainクラスを作成する
    指定通り作成しました

---
+++ src/main/java/com/github/finder/Main.java
@@ -0,0 +1,14 @@
+package com.github.finder;
+
+/**
+ * Hello world!
+ *
+ */
+ public class Main{
+     public Main(String[] arguments){
+     }
+
+     public static void main(String[] args){
+         new Main(args);
+     }
+ }
